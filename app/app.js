bcda8893519118fd42d00831e37860415b501c44
leandrojaviercepeda <leandrojaviercepeda@gmail.com>
2022-03-11 20:41:34 -0300

    m4u4:
    en progreso:
    * Programar una ruta que varie el contenido de su respuesta en base a un parámetro recibido.
    
    * Subir a un repositorio publico en GitHub y compartir la URL del mismo.

--- app/app.js
+++ app/app.js
@@ -3,11 +3,20 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var loginRouter = require('./routes/login');
+var logoutRouter = require('./routes/logout');
 
 var app = express();
+
+app.use(session({
+  secret: '15d35a1576bd415e7a57277f4a182f1b',
+  resave: false,
+  saveUninitialized: true,
+}));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,6 +30,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/login', loginRouter);
+app.use('/logout', logoutRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.redirect('/login');
+});
+
+router.get('/home', function(req, res, next) {
+  res.render('index', { title: 'Home' });
 });
 
 module.exports = router;
